d15fa7011d54999f96897e9bc7fba23c687a8042
Simon Struck <simon@simonscode.org>
2022-04-12 16:54:54 +0200

    Add LED lighting up on start

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_main]
 #![feature(lang_items)]
 
+extern crate avr_device;
+
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
 
@@ -9,9 +11,19 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern fn main() {
-    loop {
+    let peripherals = unsafe { avr_device::attiny85::Peripherals::steal() };
 
-    }
+    // shorthand for IO
+    let portb = peripherals.PORTB;
+
+    // set port 1 (LED) as output
+    portb.ddrb.write(|w| w.pb1().set_bit());
+
+    // set port 1 (LED) on
+    portb.portb.write(|w| w.pb1().set_bit());
+
+    // do nothing forever
+    loop {}
 }
 
 #[panic_handler]
