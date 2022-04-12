ff6c56a109c4b3d7730798e66de81563b36409d2
Simon Struck <simon@simonscode.org>
2022-04-12 17:05:45 +0200

    WIP: Interrupt Timer

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,26 @@ extern "C" fn eh_personality() {}
 
 use core::panic::PanicInfo;
 
+static mut A: bool = false;
+
+pub extern fn isr() {
+    unsafe { A = !A };
+}
+
 #[no_mangle]
 pub extern fn main() {
     let peripherals = unsafe { avr_device::attiny85::Peripherals::steal() };
+    peripherals.WDT.wdtcr.write(|w|
+        w.wdif().set_bit()
+            .wdie().set_bit()
+            .wde().set_bit()
+            .wdpl().cycles_64k());
+
+    // peripherals.EXINT.gifr.write(|w| w.)
+
+    unsafe  {
+        let prt =
+    }
 
     // shorthand for IO
     let portb = peripherals.PORTB;
@@ -23,7 +40,11 @@ pub extern fn main() {
     portb.portb.write(|w| w.pb1().set_bit());
 
     // do nothing forever
-    loop {}
+    loop {
+        unsafe {
+            portb.portb.write(|w| w.pb1().bit(A));
+        }
+    }
 }
 
 #[panic_handler]
