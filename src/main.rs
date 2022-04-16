91a69aa30f6cced12ef9153df787bf902b31e269
Simon Struck <simon@simonscode.org>
2022-04-16 02:46:56 +0200

    WIP: Attempt to get watchdog working more than once

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,55 @@
 #![no_main]
 #![feature(lang_items)]
 #![feature(abi_avr_interrupt)]
+#![feature(llvm_asm)]
 
 extern crate avr_device;
 
 use core::panic::PanicInfo;
+use avr_device::attiny85::Peripherals;
 
 static mut A: bool = false;
 
+#[allow(non_snake_case)]
 #[avr_device::interrupt(attiny85)]
 unsafe fn WDT() {
+    let peripherals = Peripherals::steal();
+    peripherals.WDT.wdtcr.write(|w| w
+        .wdce().set_bit()
+        .wde().set_bit()
+        .wdif().set_bit()
+    );
     A = !A;
+    peripherals.PORTB.portb.write(|w| w.pb1().bit(A));
 }
 
-#[no_mangle]
-pub extern fn main() {
-    let peripherals = unsafe { avr_device::attiny85::Peripherals::steal() };
+#[avr_device::entry]
+fn main() -> ! {
+    let peripherals = unsafe { Peripherals::steal() };
+
+    // Clear watchdog reset
+    peripherals.CPU.mcusr.write(|w|w.wdrf().clear_bit());
+
+    // Unlock watchdog changes
     peripherals.WDT.wdtcr.write(|w|
         w
-            .wdif().set_bit()
-            .wdie().set_bit()
-            .wdpl().cycles_256k()
-            .wde().set_bit()
+            .wdce().set_bit() // Watchdog Change Enable
+            .wde().set_bit() // Watchdog Enable
     );
+
+    // Configure watchdog
+    peripherals.WDT.wdtcr.write(|w|
+        w
+            .wdif().set_bit() //
+            .wdie().set_bit() //
+            .wdpl().cycles_64k() //
+            .wde().set_bit() //
+    );
+
+    // Enable interrupts
+    unsafe {
+        llvm_asm!("sei");
+    }
 
     // shorthand for IO
     let portb = peripherals.PORTB;
@@ -34,13 +61,8 @@ pub extern fn main() {
     // set port 1 (LED) on
     portb.portb.write(|w| w.pb1().set_bit());
 
-
-
     // do nothing forever
-    loop {
-        // let x = unsafe { A };
-        // portb.portb.write(|w| w.pb1().bit(x));
-    }
+    loop {}
 }
 
 #[panic_handler]
